9c37e4e3506065cdb6f8b4ff1478fcfd8609b456
Kartik Nair <kartiknair1911@gmail.com>
2020-05-07 14:28:32 +0400

    Debugging npm packages from github

--- dist/index.js
+++ dist/index.js
@@ -128,7 +128,7 @@ class RichMarkdownEditor extends React.PureComponent {
             return (React.createElement(Flex_1.default, { onKeyDown: onKeyDown, style: style, className: className, align: "flex-start", justify: "center", column: true },
                 React.createElement(styled_components_1.ThemeProvider, { theme: theme },
                     React.createElement(React.Fragment, null,
-                        React.createElement(StyledEditor, { readOnly: readOnly, ref: ref => (this.element = ref) }),
+                        React.createElement(StyledEditor, { readOnly: readOnly, ref: (ref) => (this.element = ref) }),
                         !readOnly && this.view && (React.createElement(React.Fragment, null,
                             React.createElement(FloatingToolbar_1.default, { view: this.view, commands: this.commands, onSearchLink: this.props.onSearchLink, onClickLink: this.props.onClickLink, tooltip: tooltip }),
                             React.createElement(BlockMenu_1.default, { view: this.view, commands: this.commands, isActive: this.state.blockMenuOpen, search: this.state.blockMenuSearch, onClose: this.handleCloseBlockMenu, uploadImage: this.props.uploadImage, onImageUploadStart: this.props.onImageUploadStart, onImageUploadStop: this.props.onImageUploadStop, onShowToast: this.props.onShowToast })))))));
@@ -312,10 +312,10 @@ class RichMarkdownEditor extends React.PureComponent {
             state: this.createState(),
             editable: () => !this.props.readOnly,
             nodeViews: this.nodeViews,
-            dispatchTransaction: transaction => {
+            dispatchTransaction: (transaction) => {
                 const { state, transactions } = this.view.state.applyTransaction(transaction);
                 this.view.updateState(state);
-                if (transactions.some(tr => tr.docChanged)) {
+                if (transactions.some((tr) => tr.docChanged)) {
                     this.handleChange();
                 }
                 this.forceUpdate();
@@ -344,17 +344,17 @@ RichMarkdownEditor.defaultProps = {
     },
     onImageUploadStop: () => {
     },
-    onClickLink: href => {
+    onClickLink: (href) => {
         window.open(href, "_blank");
     },
     extensions: [],
     tooltip: "span",
 };
 const StyledEditor = styled_components_1.default("div") `
-  color: ${props => props.theme.text};
-  background: ${props => props.theme.background};
-  font-family: ${props => props.theme.fontFamily};
-  font-weight: ${props => props.theme.fontWeight};
+  color: ${(props) => props.theme.text};
+  background: ${(props) => props.theme.background};
+  font-family: ${(props) => props.theme.fontFamily};
+  font-weight: ${(props) => props.theme.fontWeight};
   font-size: 1em;
   line-height: 1.7em;
   width: 100%;
@@ -386,7 +386,7 @@ const StyledEditor = styled_components_1.default("div") `
     text-align: center;
 
     img {
-      pointer-events: ${props => (props.readOnly ? "initial" : "none")};
+      pointer-events: ${(props) => (props.readOnly ? "initial" : "none")};
     }
   }
 
@@ -405,7 +405,7 @@ const StyledEditor = styled_components_1.default("div") `
   }
 
   .ProseMirror-selectednode {
-    outline: 2px solid ${props => props.theme.selected};
+    outline: 2px solid ${(props) => props.theme.selected};
   }
 
   /* Make sure li selections wrap around markers */
@@ -421,7 +421,7 @@ const StyledEditor = styled_components_1.default("div") `
     right: -2px;
     top: -2px;
     bottom: -2px;
-    border: 2px solid ${props => props.theme.selected};
+    border: 2px solid ${(props) => props.theme.selected};
     pointer-events: none;
   }
 
@@ -435,11 +435,11 @@ const StyledEditor = styled_components_1.default("div") `
     cursor: default;
 
     &:not(.placeholder):before {
-      display: ${props => (props.readOnly ? "none" : "block")};
+      display: ${(props) => (props.readOnly ? "none" : "block")};
       position: absolute;
-      font-family: ${props => props.theme.fontFamilyMono};
-      color: ${props => props.theme.textSecondary};
-      font-size: 13px;
+      font-family: ${(props) => props.theme.fontFamilyMono};
+      color: ${(props) => props.theme.textSecondary};
+      font-size: 0.8rem;
       left: -24px;
     }
   }
@@ -452,7 +452,7 @@ const StyledEditor = styled_components_1.default("div") `
   h6:not(.placeholder):before { content: "H6"; }
 
   .heading-name {
-    color: ${props => props.theme.text};
+    color: ${(props) => props.theme.text};
 
     &:hover {
       text-decoration: none;
@@ -469,40 +469,40 @@ const StyledEditor = styled_components_1.default("div") `
 
   .heading-anchor {
     opacity: 0;
-    display: ${props => (props.readOnly ? "block" : "none")};
-    color: ${props => props.theme.textSecondary};
+    display: ${(props) => (props.readOnly ? "block" : "none")};
+    color: ${(props) => props.theme.textSecondary};
     cursor: pointer;
     background: none;
     border: 0;
     outline: none;
-    padding: 2px 12px 2px 4px;
+    padding: 0.125rem 0.75rem 0.125rem 0.25rem;
     margin: 0;
     position: absolute;
     transition: opacity 100ms ease-in-out;
-    font-family: ${props => props.theme.fontFamilyMono};
-    font-size: 22px;
+    font-family: ${(props) => props.theme.fontFamilyMono};
+    font-size: 1.4rem;
     left: -1.3em;
 
     &:focus,
     &:hover {
-      color: ${props => props.theme.text};
+      color: ${(props) => props.theme.text};
     }
   }
 
   .placeholder {
     &:before {
       display: block;
-      content: ${props => (props.readOnly ? "" : "attr(data-empty-text)")};
+      content: ${(props) => (props.readOnly ? "" : "attr(data-empty-text)")};
       pointer-events: none;
       height: 0;
-      color: ${props => props.theme.placeholder};
+      color: ${(props) => props.theme.placeholder};
     }
   }
 
   blockquote {
-    border-left: 3px solid ${props => props.theme.quote};
+    border-left: 3px solid ${(props) => props.theme.quote};
     margin: 0;
-    padding-left: 10px;
+    padding-left: 0.6em;
     font-style: italic;
   }
 
@@ -513,15 +513,15 @@ const StyledEditor = styled_components_1.default("div") `
 
   p {
     position: relative;
-    margin: 0;
+    margin: 0.5em 0;
   }
 
   a {
-    color: ${props => props.theme.link};
+    color: ${(props) => props.theme.link};
   }
 
   a:hover {
-    text-decoration: ${props => (props.readOnly ? "underline" : "none")};
+    text-decoration: ${(props) => (props.readOnly ? "underline" : "none")};
   }
 
   ul,
@@ -538,22 +538,22 @@ const StyledEditor = styled_components_1.default("div") `
   ul.checkbox_list {
     list-style: none;
     padding-left: 0;
-    margin-left: -4px;
+    margin-left: -0.25em;
 
     ul.checkbox_list {
-      padding-left: 20px;
+      padding-left: 1.25em;
     }
   }
 
   ul.checkbox_list li.checked > span > p {
-    color: ${props => props.theme.textSecondary};
+    color: ${(props) => props.theme.textSecondary};
     text-decoration: line-through;
   }
 
   ul.checkbox_list li input {
-    pointer-events: ${props => (props.readOnly ? "none" : "initial")};
-    opacity: ${props => (props.readOnly ? 0.75 : 1)};
-    margin-right: 6px;
+    pointer-events: ${(props) => (props.readOnly ? "none" : "initial")};
+    opacity: ${(props) => (props.readOnly ? 0.75 : 1)};
+    margin-right: 0.4em;
   }
 
   li p:first-child {
@@ -564,21 +564,20 @@ const StyledEditor = styled_components_1.default("div") `
   hr {
     height: 0;
     border: 0;
-    border-top: 1px solid ${props => props.theme.horizontalRule};
+    border-top: 1px solid ${(props) => props.theme.horizontalRule};
   }
 
   code {
-    border-radius: 4px;
-    border: 1px solid ${props => props.theme.codeBorder};
-    padding: 3px 4px;
-    font-family: "Source Code Pro", Menlo, monospace;
+    padding: 0.2em;
+    background: ${(props) => props.theme.codeBackground};
+    font-family: ${(props) => props.theme.fontFamilyMono};
     font-size: 85%;
   }
 
   mark {
     border-radius: 1px;
-    color: ${props => props.theme.black};
-    background: ${props => props.theme.textHighlight};
+    color: ${(props) => props.theme.black};
+    background: ${(props) => props.theme.textHighlight};
   }
 
   .code-block {
@@ -595,11 +594,11 @@ const StyledEditor = styled_components_1.default("div") `
 
     &:hover {
       select {
-        display: ${props => (props.readOnly ? "none" : "inline")};
+        display: ${(props) => (props.readOnly ? "none" : "inline")};
       }
   
       button {
-        display: ${props => (props.readOnly ? "inline" : "none")};
+        display: ${(props) => (props.readOnly ? "inline" : "none")};
       }
     }
   }
@@ -607,16 +606,14 @@ const StyledEditor = styled_components_1.default("div") `
   pre {
     display: block;
     overflow-x: auto;
-    padding: 0.5em 1em;
+    padding: 1.5em 2em;
     line-height: 1.4em;
     position: relative;
-    background: ${props => props.theme.codeBackground};
-    border-radius: 4px;
-    border: 1px solid ${props => props.theme.codeBorder};
+    background: ${(props) => props.theme.codeBackground};
 
     -webkit-font-smoothing: initial;
-    font-family: ${props => props.theme.fontFamilyMono}
-    font-size: 13px;
+    font-family: ${(props) => props.theme.fontFamilyMono}
+    font-size: 0.8em;
     direction: ltr;
     text-align: left;
     white-space: pre;
@@ -629,8 +626,8 @@ const StyledEditor = styled_components_1.default("div") `
     -moz-hyphens: none;
     -ms-hyphens: none;
     hyphens: none;
-    color: ${props => props.theme.code};
-    margin: 0;
+    color: ${(props) => props.theme.code};
+    margin: 0.5em 0;
 
     code {
       background: none;
@@ -643,11 +640,11 @@ const StyledEditor = styled_components_1.default("div") `
   .token.prolog,
   .token.doctype,
   .token.cdata {
-    color: ${props => props.theme.codeComment};
+    color: ${(props) => props.theme.codeComment};
   }
 
   .token.punctuation {
-    color: ${props => props.theme.codePunctuation};
+    color: ${(props) => props.theme.codePunctuation};
   }
 
   .token.namespace {
@@ -657,34 +654,34 @@ const StyledEditor = styled_components_1.default("div") `
   .token.operator,
   .token.boolean,
   .token.number {
-    color: ${props => props.theme.codeNumber};
+    color: ${(props) => props.theme.codeNumber};
   }
 
   .token.property {
-    color: ${props => props.theme.codeProperty};
+    color: ${(props) => props.theme.codeProperty};
   }
 
   .token.tag {
-    color: ${props => props.theme.codeTag};
+    color: ${(props) => props.theme.codeTag};
   }
 
   .token.string {
-    color: ${props => props.theme.codeString};
+    color: ${(props) => props.theme.codeString};
   }
 
   .token.selector {
-    color: ${props => props.theme.codeSelector};
+    color: ${(props) => props.theme.codeSelector};
   }
 
   .token.attr-name {
-    color: ${props => props.theme.codeAttr};
+    color: ${(props) => props.theme.codeAttr};
   }
 
   .token.entity,
   .token.url,
   .language-css .token.string,
   .style .token.string {
-    color: ${props => props.theme.codeEntity};
+    color: ${(props) => props.theme.codeEntity};
   }
 
   .token.attr-value,
@@ -692,22 +689,22 @@ const StyledEditor = styled_components_1.default("div") `
   .token.control,
   .token.directive,
   .token.unit {
-    color: ${props => props.theme.codeKeyword};
+    color: ${(props) => props.theme.codeKeyword};
   }
 
   .token.function {
-    color: ${props => props.theme.codeFunction};
+    color: ${(props) => props.theme.codeFunction};
   }
 
   .token.statement,
   .token.regex,
   .token.atrule {
-    color: ${props => props.theme.codeStatement};
+    color: ${(props) => props.theme.codeStatement};
   }
 
   .token.placeholder,
   .token.variable {
-    color: ${props => props.theme.codePlaceholder};
+    color: ${(props) => props.theme.codePlaceholder};
   }
 
   .token.deleted {
@@ -715,7 +712,7 @@ const StyledEditor = styled_components_1.default("div") `
   }
 
   .token.inserted {
-    border-bottom: 1px dotted ${props => props.theme.codeInserted};
+    border-bottom: 1px dotted ${(props) => props.theme.codeInserted};
     text-decoration: none;
   }
 
@@ -729,7 +726,7 @@ const StyledEditor = styled_components_1.default("div") `
   }
 
   .token.important {
-    color: ${props => props.theme.codeImportant};
+    color: ${(props) => props.theme.codeImportant};
   }
 
   .token.entity {
@@ -739,30 +736,30 @@ const StyledEditor = styled_components_1.default("div") `
   table {
     width: 100%;
     border-collapse: collapse;
-    border-radius: 4px;
+    border-radius: 0.25em;
     margin-top: 1em;
 
     tr {
       position: relative;
-      border-bottom: 1px solid ${props => props.theme.tableDivider};
+      border-bottom: 1px solid ${(props) => props.theme.tableDivider};
     }
 
     th {
-      background: ${props => props.theme.tableHeaderBackground};
+      background: ${(props) => props.theme.tableHeaderBackground};
     }
 
     td, th {
       position: relative;
       vertical-align: top;
-      border: 1px solid ${props => props.theme.tableDivider};
+      border: 1px solid ${(props) => props.theme.tableDivider};
       position: relative;
-      padding: 4px 8px;
+      padding: 0.25em 0.5em;
       text-align: left;
-      min-width: 100px;
+      min-width: 6.25em;
     }
 
     .selectedCell {
-      background: ${props => props.readOnly ? "inherit" : props.theme.tableSelectedBackground};
+      background: ${(props) => props.readOnly ? "inherit" : props.theme.tableSelectedBackground};
     }
 
     .grip-column {
@@ -772,12 +769,12 @@ const StyledEditor = styled_components_1.default("div") `
       left: 0;
       width: 100%;
       height: 12px;
-      background: ${props => props.theme.tableDivider};
-      border-bottom: 3px solid ${props => props.theme.background};
-      display: ${props => (props.readOnly ? "none" : "block")};
+      background: ${(props) => props.theme.tableDivider};
+      border-bottom: 3px solid ${(props) => props.theme.background};
+      display: ${(props) => (props.readOnly ? "none" : "block")};
 
       &:hover {
-        background: ${props => props.theme.text};
+        background: ${(props) => props.theme.text};
       }
       &.first {
         border-top-left-radius: 3px;
@@ -786,7 +783,7 @@ const StyledEditor = styled_components_1.default("div") `
         border-top-right-radius: 3px;
       }
       &.selected {
-        background: ${props => props.theme.tableSelected};
+        background: ${(props) => props.theme.tableSelected};
       }
     }
 
@@ -797,12 +794,12 @@ const StyledEditor = styled_components_1.default("div") `
       top: 0;
       height: 100%;
       width: 12px;
-      background: ${props => props.theme.tableDivider};
-      border-right: 3px solid ${props => props.theme.background};
-      display: ${props => (props.readOnly ? "none" : "block")};
+      background: ${(props) => props.theme.tableDivider};
+      border-right: 3px solid ${(props) => props.theme.background};
+      display: ${(props) => (props.readOnly ? "none" : "block")};
 
       &:hover {
-        background: ${props => props.theme.text};
+        background: ${(props) => props.theme.text};
       }
       &.first {
         border-top-left-radius: 3px;
@@ -811,27 +808,27 @@ const StyledEditor = styled_components_1.default("div") `
         border-bottom-left-radius: 3px;
       }
       &.selected {
-        background: ${props => props.theme.tableSelected};
+        background: ${(props) => props.theme.tableSelected};
       }
     }
 
     .grip-table {
       cursor: pointer;
-      background: ${props => props.theme.tableDivider};
+      background: ${(props) => props.theme.tableDivider};
       width: 13px;
       height: 13px;
       border-radius: 13px;
-      border: 2px solid ${props => props.theme.background};
+      border: 2px solid ${(props) => props.theme.background};
       position: absolute;
       top: -18px;
       left: -18px;
-      display: ${props => (props.readOnly ? "none" : "block")};
+      display: ${(props) => (props.readOnly ? "none" : "block")};
 
       &:hover {
-        background: ${props => props.theme.text};
+        background: ${(props) => props.theme.text};
       }
       &.selected {
-        background: ${props => props.theme.tableSelected};
+        background: ${(props) => props.theme.tableSelected};
       }
     }
   }
@@ -863,24 +860,24 @@ const StyledEditor = styled_components_1.default("div") `
     pointer-events: none;
 
     &.left {
-      box-shadow: 16px 0 16px -16px inset rgba(0,0,0,0.25);
-      border-left: 1em solid ${props => props.theme.background};
+      box-shadow: 1em 0 1em -1em inset rgba(0,0,0,0.25);
+      border-left: 1em solid ${(props) => props.theme.background};
     }
 
     &.right {
       right: 0;
       left: auto;
-      box-shadow: -16px 0 16px -16px inset rgba(0,0,0,0.25);
+      box-shadow: -1em 0 1em -1em inset rgba(0,0,0,0.25);
     }
   }
 
   .block-menu-trigger {
-    display: ${props => (props.readOnly ? "none" : "block")};
+    display: ${(props) => (props.readOnly ? "none" : "block")};
     height: 1em;
-    color: ${props => props.theme.textSecondary};
+    color: ${(props) => props.theme.textSecondary};
     background: none;
     border-radius: 100%;
-    font-size: 30px;
+    font-size: 1.8rem;
     position: absolute;
     transform: scale(0.9);
     transition: color 150ms cubic-bezier(0.175, 0.885, 0.32, 1.275),
@@ -888,14 +885,14 @@ const StyledEditor = styled_components_1.default("div") `
     outline: none;
     border: 0;
     line-height: 1;
-    margin-top: -6px;
-    left: -34px;
+    margin-top: -0.125rem;
+    left: -2rem;
 
     &:hover,
     &:focus {
       cursor: pointer;
       transform: scale(1);
-      color: ${props => props.theme.text};
+      color: ${(props) => props.theme.text};
     }
   }
 
@@ -911,7 +908,7 @@ const StyledEditor = styled_components_1.default("div") `
     position: absolute;
     top: -2px;
     width: 20px;
-    border-top: 1px solid ${props => props.theme.cursor};
+    border-top: 1px solid ${(props) => props.theme.cursor};
     animation: ProseMirror-cursor-blink 1.1s steps(2, start) infinite;
   }
   
